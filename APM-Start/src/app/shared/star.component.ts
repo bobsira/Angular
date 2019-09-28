26ad292717e249072dac3fc34f2e97458e668ad7
bobsira <sbobfitz2@gmail.com>
2019-09-28 13:41:18 +0300

    passing data from a component using @Output

--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'pm-star',
@@ -10,7 +10,14 @@ export class StarComponent implements OnChanges {
     @Input() rating: number;
     starWidth: number;
 
+    @Output() ratingClicked: EventEmitter<string> =
+            new EventEmitter<string>();
+
     ngOnChanges(): void {
         this.starWidth = this.rating * 75 / 5;
+    }
+
+    onClick(): void {
+        this.ratingClicked.emit(`This rating ${this.rating} was clicked!`);
     }
 }
